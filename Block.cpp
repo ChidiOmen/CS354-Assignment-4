21dd297f427042ad1794622c2518658c42ce1629
Alexander You <alexyou8021@gmail.com>
2016-12-08 18:45:46 -0600

    fixed block error

--- Block.cpp
+++ Block.cpp
@@ -55,110 +55,109 @@ int Block::getID()
 
 
 void Block::buildBlock() {	
-	switch(type) {
-		case step:
+	if(type == step) {
+		blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
+		blockNode1->setPosition(0,25,position);
+		blockNode1->setScale(75,25,50);
+		blockEntity1 = blockManager->createEntity("block"+id, "Wood.mesh");
+		blockEntity1->setCastShadows(true);
+		blockNode1->attachObject(blockEntity1);
+	}
+	else if(type == ceiling) {
+		blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
+		blockNode1->setPosition(0,115,position+100);
+		blockNode1->setScale(75,100,200);
+		blockEntity1 = blockManager->createEntity("block"+id, "Brick.mesh");
+		blockEntity1->setCastShadows(true);
+		blockNode1->attachObject(blockEntity1);
+		
+		blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
+		blockNode2->setPosition(-175,105,position+100);
+		blockNode2->setScale(40,25,200);
+		blockEntity2 = blockManager->createEntity("blockleft"+id, "Brick.mesh");
+		blockEntity2->setCastShadows(true);
+		blockNode2->setOrientation(Ogre::Quaternion((Ogre::Radian)PI/2, Ogre::Vector3(0.0, 0.0, 1.0)));
+		blockNode2->attachObject(blockEntity2);
+			
+		blockNode3 = blockManager->getRootSceneNode()->createChildSceneNode();
+		blockNode3->setPosition(175,105,position+100);
+		blockNode3->setScale(40,25,200);
+		blockEntity3 = blockManager->createEntity("blockright"+id, "Brick.mesh");
+		blockEntity3->setCastShadows(true);
+		blockNode3->setOrientation(Ogre::Quaternion((Ogre::Radian)PI/2, Ogre::Vector3(0.0, 0.0, 1.0)));
+		blockNode3->attachObject(blockEntity3);
+	}
+	else if(type == gap) {
+		if(multiplayer) {
 			blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
-			blockNode1->setPosition(0,25,position);
-			blockNode1->setScale(75,25,50);
-			blockEntity1 = blockManager->createEntity("block"+id, "Wood.mesh");
+			blockNode1->setPosition(150,100,position);
+			blockNode1->setScale(15,100,200);
+			blockEntity1 = blockManager->createEntity("block"+id, "Brick.mesh");
 			blockEntity1->setCastShadows(true);
 			blockNode1->attachObject(blockEntity1);
-		break;
-		case ceiling:
+	
+			blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
+			blockNode2->setPosition(50,100,position);
+			blockNode2->setScale(15,100,200);
+			blockEntity2 = blockManager->createEntity("blockleft"+id, "Brick.mesh");
+			blockEntity2->setCastShadows(true);
+			blockNode2->attachObject(blockEntity2);
+
+			blockNode3 = blockManager->getRootSceneNode()->createChildSceneNode();
+			blockNode3->setPosition(-50,100,position);
+			blockNode3->setScale(15,100,200);
+			blockEntity3 = blockManager->createEntity("blockmiddle"+id, "Brick.mesh");
+			blockEntity3->setCastShadows(true);
+			blockNode3->attachObject(blockEntity3);
+
+			blockNode4 = blockManager->getRootSceneNode()->createChildSceneNode();
+			blockNode4->setPosition(-150,100,position);
+			blockNode4->setScale(15,100,200);
+			blockEntity4 = blockManager->createEntity("blockright"+id, "Brick.mesh");
+			blockEntity4->setCastShadows(true);
+			blockNode4->attachObject(blockEntity4);
+		}
+		else {
 			blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
-			blockNode1->setPosition(0,115,position+100);
-			blockNode1->setScale(75,100,200);
+			blockNode1->setPosition(50,100,position);
+			blockNode1->setScale(15,100,200);
 			blockEntity1 = blockManager->createEntity("block"+id, "Brick.mesh");
+			blockEntity1->setCastShadows(true);
+			blockNode1->attachObject(blockEntity1);
+	
+			blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
+			blockNode2->setPosition(-50,100,position);
+			blockNode2->setScale(15,100,200);
+			blockEntity2 = blockManager->createEntity("blockright"+id, "Brick.mesh");
+			blockEntity2->setCastShadows(true);
+			blockNode2->attachObject(blockEntity2);
+		}
+	}
+	else if(type == rail) {
+		if(multiplayer) {
+			blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
+			blockNode1->setPosition(-100,25,position+350);
+			blockNode1->setScale(5,25,400);
+			blockEntity1 = blockManager->createEntity("block"+id, "Metal.mesh");
 			blockEntity1->setCastShadows(true);
 			blockNode1->attachObject(blockEntity1);
 
 			blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
-			blockNode2->setPosition(-175,105,position+100);
-			blockNode2->setScale(40,25,200);
-			blockEntity2 = blockManager->createEntity("blockleft"+id, "Brick.mesh");
+			blockNode2->setPosition(100,25,position+350);
+			blockNode2->setScale(5,25,400);
+			blockEntity2 = blockManager->createEntity("blockright"+id, "Metal.mesh");
 			blockEntity2->setCastShadows(true);
-			blockNode2->setOrientation(Ogre::Quaternion((Ogre::Radian)PI/2, Ogre::Vector3(0.0, 0.0, 1.0)));
 			blockNode2->attachObject(blockEntity2);
-			
-			blockNode3 = blockManager->getRootSceneNode()->createChildSceneNode();
-			blockNode3->setPosition(175,105,position+100);
-			blockNode3->setScale(40,25,200);
-			blockEntity3 = blockManager->createEntity("blockright"+id, "Brick.mesh");
-			blockEntity3->setCastShadows(true);
-			blockNode3->setOrientation(Ogre::Quaternion((Ogre::Radian)PI/2, Ogre::Vector3(0.0, 0.0, 1.0)));
-			blockNode3->attachObject(blockEntity3);
-		break;
-		case gap:
-			if(multiplayer) {
-				blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode1->setPosition(150,100,position);
-				blockNode1->setScale(15,100,200);
-				blockEntity1 = blockManager->createEntity("block"+id, "Brick.mesh");
-				blockEntity1->setCastShadows(true);
-				blockNode1->attachObject(blockEntity1);
-	
-				blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode2->setPosition(50,100,position);
-				blockNode2->setScale(15,100,200);
-				blockEntity2 = blockManager->createEntity("blockright"+id, "Brick.mesh");
-				blockEntity2->setCastShadows(true);
-				blockNode2->attachObject(blockEntity2);
-
-				blockNode3 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode3->setPosition(-50,100,position);
-				blockNode3->setScale(15,100,200);
-				blockEntity3 = blockManager->createEntity("blockm"+id, "Brick.mesh");
-				blockEntity3->setCastShadows(true);
-				blockNode3->attachObject(blockEntity1);
-	
-				blockNode4 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode4->setPosition(-150,100,position);
-				blockNode4->setScale(15,100,200);
-				blockEntity4 = blockManager->createEntity("blockmright"+id, "Brick.mesh");
-				blockEntity4->setCastShadows(true);
-				blockNode4->attachObject(blockEntity4);
-			}
-			else {
-				blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode1->setPosition(50,100,position);
-				blockNode1->setScale(15,100,200);
-				blockEntity1 = blockManager->createEntity("block"+id, "Brick.mesh");
-				blockEntity1->setCastShadows(true);
-				blockNode1->attachObject(blockEntity1);
-	
-				blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode2->setPosition(-50,100,position);
-				blockNode2->setScale(15,100,200);
-				blockEntity2 = blockManager->createEntity("blockright"+id, "Brick.mesh");
-				blockEntity2->setCastShadows(true);
-				blockNode2->attachObject(blockEntity2);
-			}
-		break;
-		case rail:
-			if(multiplayer) {
-				blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode1->setPosition(-100,25,position+350);
-				blockNode1->setScale(5,25,400);
-				blockEntity1 = blockManager->createEntity("block"+id, "Metal.mesh");
-				blockEntity1->setCastShadows(true);
-				blockNode1->attachObject(blockEntity1);
-
-				blockNode2 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode2->setPosition(100,25,position+350);
-				blockNode2->setScale(5,25,400);
-				blockEntity2 = blockManager->createEntity("blockm"+id, "Metal.mesh");
-				blockEntity2->setCastShadows(true);
-				blockNode2->attachObject(blockEntity2);
-			}
-			else {
-				blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
-				blockNode1->setPosition(0,25,position+350);
-				blockNode1->setScale(5,25,400);
-				blockEntity1 = blockManager->createEntity("block"+id, "Metal.mesh");
-				blockEntity1->setCastShadows(true);
-				blockNode1->attachObject(blockEntity1);
-			}
-		break;
+		}
+		else {
+			blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
+			blockNode1->setPosition(0,25,position+350);
+			blockNode1->setScale(5,25,400);
+			blockEntity1 = blockManager->createEntity("block"+id, "Metal.mesh");
+			blockEntity1->setCastShadows(true);
+			blockNode1->attachObject(blockEntity1);
+		}
+	}
 		/*case sideRun: 
 			blockNode1 = blockManager->getRootSceneNode()->createChildSceneNode();
 			blockNode1->setPosition(-100,100,position+350);
@@ -167,9 +166,6 @@ void Block::buildBlock() {
 			blockEntity1->setCastShadows(true);
 			blockNode1->attachObject(blockEntity1);
 		break;*/
-	}
-
-
 }
 
 void Block::setPosition(int x, int y, int z) {
